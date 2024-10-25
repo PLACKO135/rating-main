dba3b41b17004ed0f4a26e9f870076a9395b32ac
Placko135 <papplaszlolevente@gmail.com>
2024-10-25 13:00:03 +0200

    yippe

--- src/rating/Rating.tsx
+++ src/rating/Rating.tsx
@@ -1,32 +1,68 @@
-import star from '../../images/icon-star.svg'
-import thankYou from '../../images/illustration-thank-you.svg'
+import { FormEvent, useState } from "react"
+import star from "../images/icon-star.svg"
+import thankYou from "../images/illustration-thank-you.svg"
+import { motion } from "framer-motion"
 
 const Rating = () => {
-  return (
-    <>
-      <div className="container">
-          <div className='starContainer'>
-              <img src={star} alt="" />
-          </div>
-          <h1>How did we do?</h1>
-          <p>Please let us know how we did with your supoport request. All feedback is appreciated to help us improve our offering!</p>
-          <div className='buttonContainer'>
-              {[1,2,3,4,5].map(num=>(
-                  <button type='button'>{num}</button>
-              ))}
-          </div>
-          <button type='submit' className='submit'>submit</button>
-      </div>
-      <div className="container thankYou">
-          <img src={thankYou} alt="" />
-         
-              <p className='selectedRate'>You selected 4 out of 5</p>
-          
-          <h1>Thank you!</h1>
-          <p>we appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
-      </div>
-    </>
-  )
+    const [isRate, setIsRate] = useState<boolean>(true)
+    const handleSetRate = (e: FormEvent) => {
+        e.preventDefault()
+        setIsRate(!isRate)
+    }
+    const [rate, setRate] = useState<number>(0)
+    const handleSelectRate = (rating: number) => {
+        setRate(rating)
+    }
+    return (
+        <>
+            {isRate ? (
+                <motion.form
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration:  1}}
+                exit={{opacity: 0}}
+                onSubmit={handleSetRate} className="container">
+                        <div className="star">
+                            <img src={star} alt="star" />
+                        </div>
+                        <h1>How did we do?</h1>
+                        <p>
+                            Please let us know how we did with your support
+                            request. All feedback is appreciated to help us
+                            improve our offering!
+                        </p>
+                        <div className="buttonContainer">
+                            {[1, 2, 3, 4, 5].map((num) => (
+                                <button  onClick={() => handleSelectRate(num)} className="rating" type="button">
+                                    {num}
+                                </button>
+                            ))}
+                        </div>
+                        <button type="submit" className="submit-btn">
+                            SUBMIT
+                        </button>
+                </motion.form>
+            ) : (
+                <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{opacity: 0}}
+                transition={{ duration:  1}}
+                className="container thankYou">
+                    <div>
+                        <img src={thankYou} alt="thank you" />
+                    </div>
+                    <p className="selected">You selected {rate} out of 5</p>
+                    <h1 className="thankYou">Thank you!</h1>
+                    <p>
+                        We appreciate you taking the time to give a rating. If
+                        you ever need more support, don't hesitate to get in
+                        touch!
+                    </p>
+                </motion.div>
+            )}
+        </>
+    )
 }
 
 export default Rating
